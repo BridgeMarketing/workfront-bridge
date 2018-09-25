ce3c72df10f0d005fb933d6511ed574168690838
Emiliano Canedo <emiliano.canedo@thebridgecorp.com>
2018-09-25 14:12:04 -0300

    A2019-692 update builder, project and block

---
+++ workfront_bridge/blocks/update.py
@@ -0,0 +1,45 @@
+from workfront_bridge.blocks.base import WFBlock
+from workfront_bridge.tools import datetime_to_wf_format
+
+
+class WFUpdateEmailDeployBlock(WFBlock):
+    '''
+    @summary: Use this block to update the deploy to an esp of an email project.
+    This block contains a single automatic task called "Update Email Deploy"
+    that will be excecuted in the Campaign Manager and will make the deploy of
+    the emails updated.
+    '''
+
+    template_name = 'Block - Update Email Deploy'
+
+    def __init__(self):
+        super(WFUpdateEmailDeployBlock, self).__init__(self.template_name)
+
+        req = ["wf_email_project_id"]
+        self._add_required_parameters(req)
+
+        # Block Fields :
+        self._wf_project_id = None
+        self._deploy_datetime = None
+
+    @property
+    def project_id(self):
+        return self._wf_project_id
+
+    @project_id.setter
+    def project_id(self, wpid):
+        '''
+        @summary: WF id of the project being updated
+        '''
+        self._wf_project_id = wpid
+        self.set_parameter("Update Email Deploy", "wf_email_project_id", wpid)
+
+    @project_id.setter
+    def deploy_datetime(self, dt):
+        '''
+        @summary: deploy datetime of the project being updated
+        '''
+        self._deploy_datetime = dt
+        self.set_parameter('Deploy Datetime',
+                           'Deployment Date/Time',
+                           datetime_to_wf_format(dt))
---
+++ workfront_bridge/projects/update.py
@@ -0,0 +1,16 @@
+from workfront_bridge.blocks.base import WFBlock
+
+
+class WFProjectUpdateContainer(WFBlock):
+    '''
+    @summary: Workfront Update Project Container.
+    Use this project container to update other workfront projects.
+    '''
+
+    template_name = "Base Project Container - Update"
+
+    def __init__(self, prj_name):
+        super(WFProjectUpdateContainer, self).__init__(self.template_name,
+                                                       name=prj_name)
+        #self._add_required_parameters([])
+        #self._add_optional_parameters([])
