594232ac87c07dfde8bb11388082686a71fa66c7
Harsh Taunk <harsh.taunk@thebridgecorp.com>
2018-10-03 14:40:28 -0400

    Adding and updating b2c project and blocks

--- workfront_bridge/blocks/data/b2c.py
+++ workfront_bridge/blocks/data/b2c.py
@@ -12,9 +12,13 @@ class WFB2CBlock(WFBlock):
         super(WFB2CBlock, self).__init__(self.template_name)
 
         req = ['Count Id']
-        self._add_required_parameters(req)
+        optional_parameters = ['Suppression File Path']
 
         self._count_id = None
+        self._suppression_file_path = None
+
+        self._add_required_parameters(req)
+        self._add_optional_parameters(optional_parameters)
 
     @property
     def count_id(self):
@@ -24,3 +28,12 @@ class WFB2CBlock(WFBlock):
     def count_id(self, idd):
         self._count_id = idd
         self.set_parameter('Pull Data', 'Count Id', idd)
+
+    @property
+    def suppression_file_path(self):
+        return self._suppression_file_path
+
+    @suppression_file_path.setter
+    def suppression_file_path(self, sup_file_path):
+        self._suppression_file_path = sup_file_path
+        self.set_parameter('Suppression', 'Suppression File Path', sup_file_path)
--- workfront_bridge/projects/b2c_builder.py
+++ workfront_bridge/projects/b2c_builder.py
@@ -17,8 +17,31 @@ class B2CProjectBuilder(object):
         self.project_name = project_name
         self.wf = wf
 
+        # Project
+        self.audience_id = None
+        self.audience_file_path = None
+        self.data_task_id = None
+        self.suppression_task_ids = []
+
         # Block
         self.count_id = None
+        self.suppression_file_path = None
+
+    def set_audience_id(self, audience_id):
+        self.audience_id = audience_id
+        return self
+
+    def set_audience_file_path(self, audience_file_path):
+        self.audience_file_path = audience_file_path
+        return self
+
+    def set_data_task_id(self, data_task_id):
+        self.data_task_id = data_task_id
+        return self
+
+    def set_suppression_task_ids(self, suppression_task_ids):
+        self.suppression_task_ids = suppression_task_ids
+        return self
 
     def set_count_id(self, count_id):
         self.count_id = count_id
@@ -27,6 +50,10 @@ class B2CProjectBuilder(object):
     def _check_viability(self):
         if self.count_id is None:
             raise WFBrigeException("{} is required".format("count_id"))
+
+    def set_suppression_file_path(self, suppression_file_path):
+        self.suppression_file_path = suppression_file_path
+        return self
 
     def build(self):
         """
@@ -37,11 +64,17 @@ class B2CProjectBuilder(object):
         """
         self._check_viability()
 
+        # Project
         project = WFProjectB2CContainer(self.project_name)
+        project.audience_id = self.audience_id
+        project.audience_file_path = self.audience_file_path
+        project.data_task_id = self.data_task_id
+        project.suppression_task_ids = self.suppression_task_ids
 
         # Block
         block = WFB2CBlock()
         block.count_id = self.count_id
+        block.suppression_file_path = self.suppression_file_path
         project.append(block)
 
         parser = WFBlockParser(self.wf)
