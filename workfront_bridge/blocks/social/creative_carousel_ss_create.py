0617760e3aa09caa72084e365e975959719aef85
Santiago Magariños <san.magarinos@gmail.com>
2019-05-24 17:01:29 -0300

    Fix broken social blocks

--- workfront_bridge/blocks/social/creative_carousel_ss_create.py
+++ workfront_bridge/blocks/social/creative_carousel_ss_create.py
@@ -10,7 +10,7 @@ class WFSocialCreativeCarouselSlideshowCreateBlock(WFBlock):
 
     block_params = {
         'Carousel/Slideshow Create': [
-            ('Social Carousel/Slideshow', 'carousel_or_slideshow', True, lambda v: v.capitalize),
+            ('Social Carousel/Slideshow', 'carousel_or_slideshow', True, lambda v: v.capitalize()),
             ('Social Creative Message', 'message', False),
             ('Social Advertiser Website URL', 'advertiser_website_url', False),
             ('FB/Instagram Call to Action', 'fb_call_to_action', False),
