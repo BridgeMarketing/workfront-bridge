7ade5fd10e155dec81fe1d9511da33177c9d9aa2
harsh <harshvtaunk@gmail.com>
2021-02-16 15:20:03 -0500

    OAP-3270: adding blue file and esp files task

---
+++ workfront_bridge/blocks/data/list_mgmt.py
@@ -0,0 +1,10 @@
+from workfront_bridge.blocks.base import WFBlock
+
+
+class WFCreateBlueFileAndEspFiles(WFBlock):
+    """
+    @summary: Use this block to add a Review Data task in a data project.
+    """
+
+    template_name = 'Block - Data - Blue File'
+
