ed6802a385711fd8d5621877a0cae200ea472622
Harsh Taunk <harsh.taunk@thebridgecorp.com>
2021-06-10 14:50:16 -0400

    Update Flag for manual ongage process

--- workfront_bridge/projects/email_builder.py
+++ workfront_bridge/projects/email_builder.py
@@ -272,7 +272,7 @@ class EmailProjectBuilder(ProjectBuilder):
         if self.live_seed_list is not None:
             project.live_seed_list = self.live_seed_list
 
-        if self.provider and self.provider.lower() == 'manual_ongage':
+        if self.provider and self.provider.lower() in ('ongage', 'manual_ongage'):
             push_dwh_el = WFEmailPushToDWHAndEL()
             project.append(push_dwh_el)
         else:
